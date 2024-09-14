2f82644191cca30c3616eb9736ad65f58c758cf3
DefRiZ <1andrewvakulenko@gmail.com>
2024-09-14 08:14:22 +0300

    refacoring code

--- components/shared/choose-pizza-form.tsx
+++ components/shared/choose-pizza-form.tsx
@@ -12,7 +12,8 @@ import {
   pizzaTypes,
 } from "@/constants/pizza";
 import { Ingridient, ProductItem } from "@prisma/client";
-import { useSet } from "react-use";
+import { usePizzaOptions } from "@/hooks";
+import { getPizzaDetails } from "@/lib/get-pizza-details";
 
 interface Props {
   imageUrl: string;
@@ -31,49 +32,23 @@ export const ChoosePizzaForm: React.FC<Props> = ({
   items,
   onClickAddCart,
 }) => {
-  const [size, setSize] = React.useState<PizzaSize>(30);
-  const [type, setType] = React.useState<PizzaType>(1);
-  const [selectedIngr, { toggle: addIngridient }] = useSet(new Set<number>([]));
-
+  const {
+    size,
+    type,
+    selectedIngr,
+    availablePizzaSizes,
+    setSize,
+    setType,
+    addIngridient,
+  } = usePizzaOptions(items);
   //Формуємо деталі піци
-  const textDetaills = `${size} см, ${mapPizzaType[type]} тісто`;
-
-  //Формуємо ціну піци
-  const pizzaPrice =
-    items.find((item) => item.pizzaType === type && item.size === size)
-      ?.price || 0;
-  const totalIngrPrice = ingredients
-    .filter((item) => selectedIngr.has(item.id))
-    .reduce((acc, item) => acc + item.price, 0);
-  const totalPrice = pizzaPrice + totalIngrPrice;
-
-  // Формуємо список доступних розмірів для поточного типу піци
-  // availablePizza - список доступних піц з поточним типом
-  const availablePizza = items.filter((item) => item.pizzaType === type);
-
-  // availablePizzaSizes - список доступних розмірів
-  const availablePizzaSizes = pizzaSizes.map((item) => {
-    return {
-      name: item.name,
-      value: item.value,
-      // розмір недоступний, якщо не знайдена піца з поточним розміром
-      disabled: !availablePizza.some(
-        (pizza) => Number(pizza.size) === Number(item.value)
-      ),
-    };
-  });
-
-  React.useEffect(() => {
-    const isAvailableSize = availablePizzaSizes.find(
-      (item) => Number(item.value) === size && !item.disabled
-    );
-
-    const availableSize = availablePizzaSizes.find((item) => !item.disabled);
-
-    if (!isAvailableSize && availableSize) {
-      setSize(Number(availableSize.value) as PizzaSize);
-    }
-  }, [type]);
+  const { textDetaills, totalPrice } = getPizzaDetails(
+    type,
+    size,
+    items,
+    ingredients,
+    selectedIngr
+  );
 
   const onClickConsole = () => {
     console.log({
